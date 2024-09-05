1215269d2bb428aefbe427167106510e880dbeb3
Rodrigo Ferraro <neuroticks@gmail.com>
2024-09-05 19:59:30 +0000

    corrigido importação do Model do Sequelize

--- src/infrastructure/db/sequelize/model/product.model.ts
+++ src/infrastructure/db/sequelize/model/product.model.ts
@@ -1,5 +1,4 @@
-import { Model } from "sequelize";
-import { Column, PrimaryKey, Table } from "sequelize-typescript";
+import { Model, Column, PrimaryKey, Table } from "sequelize-typescript";
 
 @Table({
     tableName: "products",
