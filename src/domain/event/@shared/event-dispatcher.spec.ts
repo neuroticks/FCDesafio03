c1101a205d3fff75d23b479380c053dc2b1354aa
Rodrigo Ferraro <neuroticks@gmail.com>
2024-09-14 16:12:47 +0000

    DomainEvents: Definindo teste para registrar evento

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,21 @@
+import SendEmailWhenProductIsCreated_handler from "../product/handler/send-email-when-product-created.handler";
+import EventDispatcher from "./event-dispatcher";
+
+describe("Domain Events tests", () => {
+
+    it ("should register an event handler", ()=>{
+
+        const l_eventMessage = "ProductCreatedEvent";
+
+        const l_eventDispatcher = new EventDispatcher();
+        const l_eventHandler = new SendEmailWhenProductIsCreated_handler();
+
+        l_eventDispatcher.register(l_eventMessage, l_eventHandler);
+
+        expect(l_eventDispatcher.getEventHandlers[l_eventMessage]).toBeDefined();
+
+        expect(l_eventDispatcher.getEventHandlers[l_eventMessage].length).toBe(1);
+    })
+
+    
+})
