89a201534c591c501e5c0ab477fecb3c68917792
Rodrigo Ferraro <neuroticks@gmail.com>
2024-09-14 16:22:21 +0000

    DomainEvents: Registrando o primeiro evento

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -4,12 +4,21 @@ import EventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface{
 
+    private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+    get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+        return this.eventHandlers;
+    }
+
     notify(event: EventInterface): void {
         
     }
 
     register(eventName: string, eventHandler: EventHandlerInterface): void {
-        
+        if ( !this.eventHandlers[eventName] ){
+            this.eventHandlers[eventName] = [];
+        }
+        this.eventHandlers[eventName].push(eventHandler);
     }
 
     unregister(eventName: string, eventHandler: EventHandlerInterface): void {
@@ -19,4 +28,6 @@ export default class EventDispatcher implements EventDispatcherInterface{
     unregisterAll(): void {
         
     }
+
+
 }
